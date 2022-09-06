d376f1fcb3ddf0a216496d5fd9139105e9ca1182
Andrew Udvare <audvare@gmail.com>
2022-09-06 19:42:13 -0400

    Adjustments for caching
    Print message when preloading is done
    Add routes /reset-cache and /cache-status

--- endpoint/main.py
+++ endpoint/main.py
@@ -3,20 +3,11 @@ __all__ = ('app',)
 
 from typing import Any
 import json
-# https://stackoverflow.com/questions/34145861/valueerror-failed-to-parse-cpython-sys-version-after-using-conda-command
-import sys
-
-sys.version = '3.10.1 (main, Aug 13 2022, 12:04:39) [GCC 11.3.0]'
 
 from flask import Flask, request
 from flask_caching import Cache
 from uwsgidecorators import postfork
-
 import flask
-try:
-    import uwsgi  # pylint: disable=unused-import
-except ImportError as e_:
-    raise ImportError('Running outside of uWSGI is not supported') from e_
 
 from .app_typing import EndpointRequestJSON
 
@@ -35,10 +26,9 @@ MODEL_ARGS = {
 app = Flask(__name__)
 cache = Cache(app,
               config={
-                  'CACHE_TYPE': 'FileSystemCache',
-                  'CACHE_DIR': '/theapp/cache'
+                  'CACHE_DIR': '/theapp/cache',
+                  'CACHE_TYPE': 'FileSystemCache'
               })
-cache.set('preloading', True)
 cuda_device_count = 0  # pylint: disable=invalid-name
 ENGINES: dict[str, list[Any]] = dict((key_, []) for key_ in MODEL_ARGS)
 
@@ -47,7 +37,6 @@ ENGINES: dict[str, list[Any]] = dict((key_, []) for key_ in MODEL_ARGS)
 def preload_engines():
     global ENGINES, cuda_device_count  # pylint: disable=invalid-name,global-variable-not-assigned
     # Setting the start method must be done before importing transformers.pipeines
-
     import torch
     try:
         torch.multiprocessing.set_start_method('spawn')
@@ -59,7 +48,7 @@ def preload_engines():
         for device_index in range(cuda_device_count):
             ENGINES[key].append(pipeline(*args, **kwargs, device=device_index))
             cache.set(f'{key}.{device_index}', False)
-    cache.set('preloading', False)
+    print('Done preloading engines')
 
 
 def find_next_device(key: str) -> int | None:
@@ -76,9 +65,6 @@ def clear_device(engines_key: str, index: int):
 
 def call_next_available_pipeline(engines_key: str, *args: Any,
                                  **kwargs: Any) -> Any:
-    if cache.get('preloading') is True:
-        return flask.Response(
-            json.dumps({'error': 'Still preloading. Please wait.'}), 503)
     global ENGINES, last_device_index  # pylint: disable=invalid-name,global-variable-not-assigned
     if (device_id := find_next_device(engines_key)) is None:
         return flask.Response(json.dumps({'error': 'Busy.'}), 503)
@@ -126,7 +112,7 @@ def paraphrase() -> Any:
 
 
 @app.route('/zero-shot-classification', methods=['POST'])
-def zero_shot_classification():
+def zero_shot_classification() -> Any:
     if request.json:
         content: EndpointRequestJSON = request.json
         model_args = content['model_args'] if 'model_args' in content else {}
@@ -138,3 +124,39 @@ def zero_shot_classification():
                 }
             })
     return {'error': 'Invalid input'}
+
+
+@app.route('/reset-cache')
+def reset_cache() -> dict[str, bool]:
+    values = {}
+    for key, (_, __) in MODEL_ARGS.items():
+        for i in range(cuda_device_count):
+            cache_key = f'{key}.{i}'
+            cache.set(f'{key}.{i}', False)
+            values[cache_key] = False
+    return values
+
+
+@app.route('/reset-cache', methods=['POST'])
+def reset_cache_json() -> Any:
+    if (content := request.json):
+        for key, val in content.items():
+            if not cache.has(key):
+                return flask.Response(
+                    json.dumps({'error': f'Invalid key: {key}'}), 400)
+            if not isinstance(val, bool):
+                return flask.Response(
+                    json.dumps({'error': f'Value at {key} is not a boolean'}),
+                    400)
+            cache.set(key, val)
+    return cache_status()
+
+
+@app.route('/cache-status')
+def cache_status() -> dict[str, bool]:
+    values = {}
+    for key, (_, __) in MODEL_ARGS.items():
+        for i in range(cuda_device_count):
+            cache_key = f'{key}.{i}'
+            values[cache_key] = cache.get(f'{key}.{i}')
+    return values
