f1a679e354fa0c42c6fcedf304a93594ffdd26a1
Andrew Udvare <audvare@gmail.com>
2022-09-06 16:24:57 -0400

    test: download more models in build process

--- test.py
+++ test.py
@@ -2,4 +2,7 @@ from transformers.pipelines import pipeline
 
 if __name__ == '__main__':
     # For testing, and forcing the download of the model
+    pipeline('zero-shot-classification', model='facebook/bart-large-mnli')(
+        'input', candidate_labels=['politics', 'news'], multi_label=True)
     pipeline('summarization', model='google/pegasus-cnn_dailymail')('input')
+    pipeline('summarization', model='tuner007/pegasus_paraphrase')('input')
